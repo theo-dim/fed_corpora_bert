7227b0ee01802dec7715ccf9b6b10e493f68760d
Theo Dimitrasopoulos <theonovak@mailfence.com>
2021-11-10 15:17:53 -0500

    Initial Commit.

---
+++ src/archive/GetTextDimensions.py
@@ -0,0 +1,19 @@
+import ctypes
+
+def GetTextDimensions(text, points, font):
+    class SIZE(ctypes.Structure):
+        _fields_ = [("cx", ctypes.c_long), ("cy", ctypes.c_long)]
+
+    hdc = ctypes.windll.user32.GetDC(0)
+    hfont = ctypes.windll.gdi32.CreateFontA(points, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, font)
+    hfont_old = ctypes.windll.gdi32.SelectObject(hdc, hfont)
+
+    size = SIZE(0, 0)
+    ctypes.windll.gdi32.GetTextExtentPoint32A(hdc, text, len(text), ctypes.byref(size))
+
+    ctypes.windll.gdi32.SelectObject(hdc, hfont_old)
+    ctypes.windll.gdi32.DeleteObject(hfont)
+
+    return (size.cx, size.cy)
+
+print(GetTextDimensions('Sentiment Based on Word Counting (minutes)', 12, "Helvetica"))
